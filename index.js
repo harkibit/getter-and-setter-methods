91fbf3c1f1d1dcb83892b1d50ba873d14a240159
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-14 01:08:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ class Circle{
   constructor(radius){
     this.radius = radius
   }
-  get calculate(){
-    diameter = this.radius * 2
-    Circumference = diameter * Math.PI
-    Area = Math.PI * this.radius * 2
+  get diameter(){
+    return diameter = this.radius * 2
   }
-  set raduis(newRaduis){
-    newRaduis = diameter / 2 || (Circumference/Math.PI)/2 || Area/(2*Math.PI)
+  get Circumference(){
+    return Circumference = diameter * Math.PI
+  }
+  get Area(){
+    return Area = Math.PI * this.radius * 2
+  }
+  set raduisFromDiameter(newRaduis){
+    newRaduis = diameter / 2
+    return newRaduis
+  }
+  set raduisFromCircumference(newRaduis){
+    newRaduis = (Circumference/Math.PI)/2 
+    return newRaduis
+  }
+  set raduisFromArea(newRaduis){
+    newRaduis = Area/(2*Math.PI)
     return newRaduis
   }
 }
