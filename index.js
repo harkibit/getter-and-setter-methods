e04599dadb7774580dca7d02e21c499e693ab475
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-14 01:02:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ class Circle{
   constructor(radius){
     this.radius = radius
   }
-
+  get calculate(){
+    diameter = this.radius * 2
+    Circumference = diameter * Math.PI 
+    Area = Math.PI * this.radius * 2 
+  }
+  set 
 }
